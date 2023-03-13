b83202360459365769a99c0444995432338501a6
HeYi <20301038@bjtu.edu.cn>
2023-03-13 14:38:58 +0800

    上传后端初始化代码

---
+++ backend/SmartWarehouse/src/main/java/com/sepbf/smartwarehouse/SmartWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.sepbf.smartwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWarehouseApplication.class, args);
+    }
+
+}
---
+++ backend/SmartWarehouse/src/test/java/com/sepbf/smartwarehouse/SmartWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sepbf.smartwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
