db72a41ed9aaa8d801573e6c01b806cd5a7cc19b
He Yi <85550446+heyi755@users.noreply.github.com>
2023-03-22 15:37:12 +0800

    重新编写实现类，统一返回值

--- backend/SmartWarehouse/src/main/java/com/sepbf/smartwarehouse/service/CartService.java
+++ backend/SmartWarehouse/src/main/java/com/sepbf/smartwarehouse/service/CartService.java
@@ -1,39 +1,69 @@
 package com.sepbf.smartwarehouse.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sepbf.smartwarehouse.config.Result;
+import com.sepbf.smartwarehouse.config.Result.ResultCode;
 import com.sepbf.smartwarehouse.pojo.Cart;
+import com.sepbf.smartwarehouse.service.CartService;
+import org.springframework.stereotype.Service;
 
-/**
- * CartService接口，继承自MyBatis-Plus提供的IService接口
- */
-public interface CartService extends IService<Cart> {
+@Service
+public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements CartService {
 
     /**
      * 根据ID查询Cart信息
      * @param id Cart的ID
-     * @return 查询到的Cart对象，如果查询失败返回null
+     * @return 查询到的Cart对象，如果查询失败返回错误结果对象
      */
-    Cart getCartById(Integer id);
+    @Override
+    public Result<Cart> getCartById(Integer id) {
+        Cart cart = this.getById(id);
+        if (cart == null) {
+            return Result.error(ResultCode.NOT_FOUND, "Cart not found");
+        }
+        return Result.success(cart);
+    }
 
     /**
      * 新增一条Cart记录
      * @param cart 要新增的Cart对象
-     * @return 返回值为true表示新增成功，false表示新增失败
+     * @return 返回值为成功结果对象或失败结果对象
      */
-    boolean addCart(Cart cart);
+    @Override
+    public Result<Void> addCart(Cart cart) {
+        boolean isSuccess = this.save(cart);
+        if (!isSuccess) {
+            return Result.failure();
+        }
+        return Result.success();
+    }
 
     /**
      * 更新一条Cart记录
      * @param cart 要更新的Cart对象
-     * @return 返回值为true表示更新成功，false表示更新失败
+     * @return 返回值为成功结果对象或失败结果对象
      */
-    boolean updateCart(Cart cart);
+    @Override
+    public Result<Void> updateCart(Cart cart) {
+        boolean isSuccess = this.updateById(cart);
+        if (!isSuccess) {
+            return Result.failure();
+        }
+        return Result.success();
+    }
 
     /**
      * 根据ID删除一条Cart记录
      * @param id Cart的ID
-     * @return 返回值为true表示删除成功，false表示删除失败
+     * @return 返回值为成功结果对象或失败结果对象
      */
-    boolean deleteCart(Integer id);
-}
+    @Override
+    public Result<Void> deleteCart(Integer id) {
+        boolean isSuccess = this.removeById(id);
+        if (!isSuccess) {
+            return Result.failure();
+        }
+        return Result.success();
+    }
 
+}
