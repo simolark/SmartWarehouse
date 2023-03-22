907a535ed2d26b0fa4c8a7f444e234e989fbbd0a
He Yi <85550446+heyi755@users.noreply.github.com>
2023-03-22 15:39:26 +0800

    更新service，统一返回值

--- backend/SmartWarehouse/src/main/java/com/sepbf/smartwarehouse/service/GoodsService.java
+++ backend/SmartWarehouse/src/main/java/com/sepbf/smartwarehouse/service/GoodsService.java
@@ -1,40 +1,70 @@
 package com.sepbf.smartwarehouse.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sepbf.smartwarehouse.config.Result;
+import com.sepbf.smartwarehouse.config.Result.ResultCode;
 import com.sepbf.smartwarehouse.pojo.Goods;
+import com.sepbf.smartwarehouse.service.GoodsService;
+import org.springframework.stereotype.Service;
 
-/**
- * GoodsService接口，继承自MyBatis-Plus提供的IService接口
- */
-public interface GoodsService extends IService<Goods> {
+@Service
+public class GoodsServiceImpl extends ServiceImpl<GoodsMapper, Goods> implements GoodsService {
 
     /**
      * 根据ID查询Goods信息
      * @param id Goods的ID
-     * @return 查询到的Goods对象，如果查询失败返回null
+     * @return 查询到的Goods对象，如果查询失败返回错误结果对象
      */
-    Goods getGoodsById(Integer id);
-
+    @Override
+    public Result<Goods> getGoodsById(Integer id) {
+        Goods goods = this.getById(id);
+        if (goods == null) {
+            return Result.error(ResultCode.NOT_FOUND, "Goods not found");
+        }
+        return Result.success(goods);
+    }
 
     /**
      * 新增一条Goods记录
      * @param goods 要新增的Goods对象
-     * @return 返回值为true表示新增成功，false表示新增失败
+     * @return 返回值为成功结果对象或失败结果对象
      */
-    boolean addGoods(Goods goods);
+    @Override
+    public Result<Void> addGoods(Goods goods) {
+        boolean isSuccess = this.save(goods);
+        if (!isSuccess) {
+            return Result.failure();
+        }
+        return Result.success();
+    }
 
     /**
      * 更新一条Goods记录
-     * @param goods 要更新的Cart对象
-     * @return 返回值为true表示更新成功，false表示更新失败
+     * @param goods 要更新的Goods对象
+     * @return 返回值为成功结果对象或失败结果对象
      */
-    boolean updateGoods(Goods goods);
-
+    @Override
+    public Result<Void> updateGoods(Goods goods) {
+        boolean isSuccess = this.updateById(goods);
+        if (!isSuccess) {
+            return Result.failure();
+        }
+        return Result.success();
+    }
 
     /**
      * 根据ID删除一条Goods记录
      * @param id Goods的ID
-     * @return 返回值为true表示删除成功，false表示删除失败
+     * @return 返回值为成功结果对象或失败结果对象
      */
-    boolean deleteGoods(Integer id);
+    @Override
+    public Result<Void> deleteGoods(Integer id) {
+        boolean isSuccess = this.removeById(id);
+        if (!isSuccess) {
+            return Result.failure();
+        }
+        return Result.success();
+    }
+
 }
+
